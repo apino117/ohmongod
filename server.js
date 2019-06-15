8f055287fc249d9f5b7a552717a1fa4b0af34573
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-06-15 12:36:37 -0400

    got get route saving to db

--- server.js
+++ server.js
@@ -52,47 +52,48 @@ app.get("/scrape", function (req, res) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
 
-        var results = [];
+        // var results = [];
 
         // Select each element in the HTML body from which you want information.
         // NOTE: Cheerio selectors function similarly to jQuery's selectors,
         // but be sure to visit the package's npm page to see how it works
-        $("content-card-content").each(function (i, element) {
+        // $(".content-card-content").each(function (i, element) {
 
-            var title = $(element).text();
-            var link = $(element).find("a").attr("href");
+        //     var title = $(element).find("a").text();
+        //     var link = $(element).find("a").attr("href");
 
-            // Save these results in an object that we'll push into the results array we defined earlier
-            results.push({
-                title: title,
-                link: link
-            });
-        });
+
+
+        //     // Save these results in an object that we'll push into the results array we defined earlier
+        //     results.push({
+        //         title: title,
+        //         link: link
+        //     });
+
+        //     console.log(results);
+        // });
 
         // Now, we grab every h2 within an article tag, and do the following:
-        // $("content-card-content").each(function (i, element) {
-        //     // Save an empty result object
-        //     var result = {};
+        $(".content-card-content").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
 
-        //     // Add the text and href of every link, and save them as properties of the result object
-        //     result.title = $(this)
-        //         .children("a")
-        //         .text();
-        //     result.link = $(this)
-        //         .children("a")
-        //         .attr("href");
+            // Add the text and href of every link, and save them as properties of the result object
+            result.headline = $(element).find("h3").find("a").text();
+            result.link = $(element).find("a").attr("href");
+            result.summary = $(element).find("p").find("a").text();
 
-        //     // Create a new Article using the `result` object built from scraping
-        //     db.Article.create(result)
-        //         .then(function (dbArticle) {
-        //             // View the added result in the console
-        //             console.log(dbArticle);
-        //         })
-        //         .catch(function (err) {
-        //             // If an error occurred, log it
-        //             console.log(err);
-        //         });
-        // });
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
+        });
 
         // Send a message to the client
         res.send("Scrape Complete");
